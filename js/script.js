f152097e14660f76091babc66314370a7c6d063a
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-06-03 21:57:30 -0400

    added button functionality for city search history

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 let city;
+let searchHistory = $('#history');
 const APIKey = 'a907d16219204ccff8dfbfc9285d5e56';
 let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
 let citySearch = $('#citySearch');
 let search = $('#search');
+let cityName = $('#city');
+let temp = $('#temp');
+let wind = $('#wind');
+let hum = $('#hum');
+let UV = $('#UV');
+let date = moment();
 
 search.click(function(){
     city = citySearch.val();
     console.log(city);
     getAPI(queryURL);
+    addCity();
+    loadCity();
 });
 
 function getAPI(queryURL){
@@ -16,7 +25,26 @@ function getAPI(queryURL){
         return response.json();
       })
       .then(function (data) {
+        localStorage.setItem(`${city}`, JSON.stringify(data));
         console.log(data);
       });
+}
+
+function setUI(city){
+  cityName.val(`${city} ${date}`);
+}
+
+function addCity(){
+  searchHistory.append(`<button id="${city}" class="city waves-effect waves-light btn blue">${city}</button>`);
+}
+
+function loadCity(){
+  let cityArr = $('.city');
+  cityArr.forEach(element => {
+    element.click(function(){
+      // Load weather API values from JSON string in localStorage
+      setUI();
+    })
+  });
 }
 
